cf26b387e012980341b46c8fc3abf36b1acaf8de
jack union <code@hassfeld.net>
2020-11-14 21:54:10 +0100

    Fine tuned stepper max position

--- mini-speedo.ino
+++ mini-speedo.ino
@@ -78,7 +78,7 @@ const int UPDATE_INTERVAL = 100;  // milliseconds speedo update rate
 
 //----Stepper settings and object----
 // standard X25.168 range 315 degrees at 1/3 degree steps
-#define STEPS 265*3
+#define STEPS 248*3
 // speedo type
 #define SPEEDO_RANGE 210 //cooper with 210kph full range
 // create the motor object with the maximum steps allowed
